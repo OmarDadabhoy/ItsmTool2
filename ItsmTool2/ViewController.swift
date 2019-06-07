10d4aad4bcc7338ca548b05a37a670e3775583bd
omar dadabhoy <omar.dadabhoy@gmail.com>
2019-06-07 18:33:28 -0500

    Initial Commit

---
+++ ItsmTool2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItsmTool2
+//
+//  Created by Omar Dadabhoy on 6/7/19.
+//  Copyright © 2019 Omar Dadabhoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ItsmTool2Tests/ItsmTool2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ItsmTool2Tests.swift
+//  ItsmTool2Tests
+//
+//  Created by Omar Dadabhoy on 6/7/19.
+//  Copyright © 2019 Omar Dadabhoy. All rights reserved.
+//
+
+import XCTest
+@testable import ItsmTool2
+
+class ItsmTool2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
