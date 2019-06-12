092a5ab9b572ec8f2708a1a679b72fa8e59a4a72
omar dadabhoy <omar.dadabhoy@gmail.com>
2019-06-12 16:52:24 -0500

    gives the access code and stuff if employer however there is a app crash when we try adding it to the database line we will attempt to fix that now

--- ItsmTool2/SignupViewController.swift
+++ ItsmTool2/SignupViewController.swift
@@ -21,7 +21,7 @@ class SignupViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     @IBOutlet weak var employerOrEmployeeField: UITextField!
     @IBOutlet weak var accessCodeLabel: UILabel!
     @IBOutlet weak var accessCodeorCompanyNameField: UITextField!
-    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var fullNameField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +33,7 @@ class SignupViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         //set the fields original text to the "Employer"
         self.employerOrEmployeeField.text = pickerData[0]
         self.accessCodeLabel.isHidden = true
+        self.accessCodeorCompanyNameField.isHidden = true
     }
     
     //Signs the user up for the application
@@ -44,10 +45,55 @@ class SignupViewController: UIViewController, UIPickerViewDelegate, UIPickerView
             self.present(alertController, animated: true, completion: nil)
         }
         else{
-            
             Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
                 if error == nil {
-                    self.performSegue(withIdentifier: "signupToHome", sender: self)
+                    //if the person is an employer
+                    if(self.isEmployer()){
+                        //randomly generate an accessCode for them
+                        var accessCode = self.randomString(length: 6)
+                        //create a reference to the database
+                        let ref = Database.database().reference()
+//                        var existsInDatabase: Bool = true
+//                        //run as long as the accessCode exists in the database
+//                        while(existsInDatabase){
+//                            //if the snapshot exists then re create the accessCode and try again
+//                            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+//                                if snapshot.hasChild(accessCode) {
+//                                    accessCode = self.randomString(length: 6)
+//                                } else {
+//                                    existsInDatabase = false
+//                                }
+//                            })
+//                        }
+                        //Builds a child access code and another child containing the first users info
+//                        ref.child(accessCode).child(self.email.text!).setValue(["name": self.fullNameField.text!, "Role": "Admin"])
+                        let alertController = UIAlertController(title: "Your access code is " + accessCode, message: "Give those access codes to your employees or whoever you want to add to the server so they can join your server", preferredStyle: .alert)
+                        let defaultAction = UIAlertAction(title: "Ok", style: .cancel
+                            , handler: nil)
+                        alertController.addAction(defaultAction)
+                        self.performSegue(withIdentifier: "signupToHome", sender: self)
+                        self.present(alertController, animated: true, completion: nil)
+                    } else {
+                        //create a reference
+                        let ref = Database.database().reference()
+                        //get the accessCode that the employee typed in
+                        let accessCode = self.accessCodeorCompanyNameField.text!
+                        //if the accessCode is registered in the database then the user is good else let them know it doesnt
+                        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                            if snapshot.hasChild(accessCode){
+                                //TODO
+                                //possibly could be flawed but should account to the list
+                                ref.child(accessCode).child(self.email.text!).setValue(["name": self.fullNameField.text!, "Role": "Employee"])
+                                self.performSegue(withIdentifier: "signupToHome", sender: self)
+                            } else{
+                                //alert the user that there is an error in their access code and it does not exist
+                                let alertController = UIAlertController(title: "Error", message: "The access code is not registered with us please type a valid access code", preferredStyle: .alert)
+                                let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+                                alertController.addAction(defaultAction)
+                                self.present(alertController, animated: true, completion: nil)
+                            }
+                        })
+                    }
                 }
                 else{
                     let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
@@ -59,6 +105,23 @@ class SignupViewController: UIViewController, UIPickerViewDelegate, UIPickerView
             }
             
         }
+    }
+    
+    //This function determines if the signer up is an employer or not
+    private func isEmployer() -> Bool {
+        //if the accessCodeLabel is hidden the user selected employer
+        if self.accessCodeLabel.isHidden{
+            return true
+            //else they selected employee
+        } else{
+            return false
+        }
+    }
+    
+    //This function randomly generates a string of a length in order to create the access code for the user
+    private func randomString(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0..<length).map{ _ in letters.randomElement()! })
     }
     
     override func didReceiveMemoryWarning() {
@@ -87,7 +150,7 @@ class SignupViewController: UIViewController, UIPickerViewDelegate, UIPickerView
             ifEmployee()
         } else{
             self.accessCodeLabel.isHidden = true
-            self.companyNameLabel.isHidden = false
+            self.accessCodeorCompanyNameField.isHidden = true
         }
     }
     
@@ -95,7 +158,7 @@ class SignupViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     func ifEmployee(){
         //reveal these fields so the employee can input his or her access code given by the employer
         self.accessCodeLabel.isHidden = false
-        self.companyNameLabel.isHidden = true
+        self.accessCodeorCompanyNameField.isHidden = false
     }
     
     /*
