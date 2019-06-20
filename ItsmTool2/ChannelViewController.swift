22a95f32b6b4d50f8693e3195a2962c01aae48b3
omar dadabhoy <omar.dadabhoy@gmail.com>
2019-06-19 19:52:27 -0500

    before messing with this hard

--- ItsmTool2/ChannelViewController.swift
+++ ItsmTool2/ChannelViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ChannelViewController: UIViewController {
 
     var accessCode: String = ""
+    @IBOutlet weak var tableViewLeadingConst: NSLayoutConstraint!
+    var tableViewIsHidden: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,27 @@ class ChannelViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func showMenu(_ sender: Any) {
+        if(tableViewIsHidden){
+            tableViewLeadingConst.constant = 0
+            tableViewIsHidden = false
+        } else {
+            tableViewLeadingConst.constant = -240
+            tableViewIsHidden = true
+        }
+        
+    }
+    
+    //Sees where the user touched so if they touch outside the menu itll dissapear 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        guard let location = touch?.location(in: self.view) else { return }
+        if !currentView.frame.contains(location) {
+            print("Tapped outside the view")
+        }else {
+            print("Tapped inside the view")
+        }
+    }
     
     
 
