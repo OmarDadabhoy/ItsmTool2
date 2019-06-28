887649a9e83d5a3cb29f60c484efc25af9d173bf
omar dadabhoy <omar.dadabhoy@gmail.com>
2019-06-27 19:38:07 -0500

    got all the incidents to show up in the table

--- ItsmTool2/IncidentsViewController.swift
+++ ItsmTool2/IncidentsViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import Firebase
 
 class IncidentsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var menuButton: UIBarButtonItem = UIBarButtonItem()
     @IBOutlet weak var tableView: UITableView!
     var tableData: [String] = []
+    let db = Firestore.firestore()
+    var tableDataValues: [Any] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.fillTableData()
         if self.revealViewController() != nil {
             print("not nil")
             menuButton = UIBarButtonItem.init(title: "Menu", style: .plain, target: self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)))
@@ -26,11 +32,27 @@ class IncidentsViewController: UIViewController, UITableViewDataSource, UITableV
             self.revealViewController().navigationItem.leftBarButtonItem = menuButton
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
-        fillTableData()
     }
     
+    //Fills the table with the incidents
     func fillTableData(){
-        
+        print(currentAccessCode)
+        let docRef = db.collection("Access Code Incidents").document(currentAccessCode)
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let data = document.data()
+                //loop through the data and add all the keys/incident names to the table data
+                for dataDoc in data! {
+                    self.tableData.append(dataDoc.key)
+                    //This will contain the values for each of the keys in tableData
+                    self.tableDataValues.append(dataDoc.value)
+                }
+                print(self.tableData)
+                self.tableView.reloadData()
+            } else {
+                print("Document does not exist")
+            }
+        }
     }
     
     //The number of rows
@@ -44,6 +66,22 @@ class IncidentsViewController: UIViewController, UITableViewDataSource, UITableV
         let text = tableData[indexPath.row]
         cell?.textLabel?.text = text
         return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(self.tableData[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        if(self.tableData[indexPath.row] == "Incidents") {
+            self.performSegue(withIdentifier: "goToIncidents", sender: self)
+        } else if(self.tableData[indexPath.row] == "Home") {
+            let channelViewController = ChannelViewController();
+            if channelViewController.viewIfLoaded?.window == nil {
+                // viewController is not visible
+                if let navController = self.navigationController{
+                    navController.popViewController(animated: true)
+                }
+            }
+        }
     }
     
     //Create a new incident
